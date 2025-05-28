2f0aa5353ea1199ef782a74e385fe4eeede6931b
Rafael <rafarrp@yahoo.com.br>
2025-05-28 03:50:55 -0300

    Atualiza app/Config/Routes.php e remove .gitignore do versionamento

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,13 +13,3 @@ $routes->resource('api/users', [
   'filter'     => 'authfilter'
 ]);
 
-// $routes->resource('api/users');
-
-// 'filter'     => 'corsAuth'
-
-// $routes->resource('api/users', ['filter' => 'authfilter']);
-
-// $routes->get('api/users', 'Users::list');
-// $routes->post('api/users/new', 'Users::create');
-// $routes->delete('api/users/(:num)', 'Users::delete');
-
